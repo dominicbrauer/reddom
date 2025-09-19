fc57464cf8be5b628bb8de0cd74f14d1d89ec40d
dominicbrauer <dominicbrauer06@proton.me>
2025-09-19 09:53:07 +0200

    feat: Add sidebar tabs, configure default redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,18 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import node from '@astrojs/node';
-
 import db from '@astrojs/db';
 
 export default defineConfig({
-  site: "https://reddom.dominicbrauer.dev",
-  output: "server",
-
-  devToolbar: {
-      enabled: false
+	site: "https://reddom.dominicbrauer.dev",
+	output: "server",
+	devToolbar: {
+		enabled: false
 	},
-
-  adapter: node({
-      mode: 'standalone'
+	adapter: node({
+		mode: 'standalone'
 	}),
-
-  integrations: [db()]
+	integrations: [db()],
+	redirects: {
+		"/": "/getting-started",
+	}
 });
---
+++ src/middleware.ts
@@ -0,0 +1,6 @@
+import { defineMiddleware } from "astro:middleware";
+
+export const onRequest = defineMiddleware((context, next) => {
+
+	return next();
+});
