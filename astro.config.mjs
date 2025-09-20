f0459a6558718bcc9a58af34c7e7bb896d599f7c
Dominic Brauer <dominicbrauer06@gmail.com>
2025-09-20 12:30:01 +0200

    fix: add astroDB back

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import db from '@astrojs/db';
+import db from "@astrojs/db";
 
 export default defineConfig({
-	site: "https://reddom.dominicbrauer.dev",
-	output: "server",
-	devToolbar: {
-		enabled: false
+  site: "https://reddom.dominicbrauer.dev",
+  output: "server",
+
+  devToolbar: {
+      enabled: false
 	},
-	adapter: node({
-		mode: 'standalone'
+
+  adapter: node({
+      mode: 'standalone'
 	}),
-	integrations: [db()],
-	redirects: {
-		"/": "/getting-started",
-	}
+
+  redirects: {
+      "/": "/getting-started",
+	},
+
+  integrations: [db()]
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,11 +1,5 @@
-import { db, Block } from 'astro:db';
+import { db } from 'astro:db';
 
 export default async function seed() {
-	await db.insert(Block).values([
-		{ name: "redstone_dust", formated_name: "Redstone Dust" },
-		{ name: "sticky_piston", formated_name: "Sticky Piston" },
-		{ name: "repeater", formated_name: "Repeater" },
-		{ name: "crafter", formated_name: "Crafter" },
-		{ name: "redstone_torch", formated_name: "Redstone Torch" },
-	]);
+
 }
