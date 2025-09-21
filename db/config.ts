758b7c914b6bf16505a15bf46b82ce27d6f8bbd6
Dominic Brauer <dominicbrauer06@gmail.com>
2025-09-21 18:52:09 +0200

    feat: Add Block table columns

--- db/config.ts
+++ db/config.ts
@@ -2,8 +2,22 @@ import { defineDb, defineTable, column } from 'astro:db';
 
 const Block = defineTable({
 	columns: {
-		name: column.text({ primaryKey: true }),
-		formated_name: column.text(),
+		// block ID
+		id_name: column.text({ primaryKey: true }),
+		// block name
+		name: column.text(),
+		// can be stacked to bundles of 16/64
+		is_stackable: column.boolean(),
+		// exact stack size if stackable
+		stack_size: column.number(),
+		// constant for how easy the block is destructable by explosions
+		blast_resistance: column.number(),
+		// constant for how easy the block is breakable
+		hardness: column.number(),
+		// covers a whole block without see-through
+		is_transparent: column.boolean(),
+		// required tool for breaking
+		tool: column.text(),
 	}
 });
 
